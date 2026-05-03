8ec0d678e086ff23a599538fb7f178e07183a1fc
Pinar <pinarbildirici87@gmail.com>
2026-05-04 00:29:07 +0200

    update habit model streak logic and refactor folders

--- Habittrackerxcode/Models /habit.swift
+++ Habittrackerxcode/Models /habit.swift
@@ -13,10 +13,46 @@ final class habit {
     var name: String
     var notes: String
     var timestamp: Date
+    var completedDates: [Date] = [] 
     
+             
+       
+            
     init(name: String = "", notes: String = "", timestamp: Date = .now) {
-        self.name = name
-        self.notes = notes
-        self.timestamp = timestamp
+                self.name = name
+                self.notes = notes
+                self.timestamp = timestamp
+                self.completedDates = []
+            }
+            
+            // Streak//
+            var currentStreak: Int {
+                guard !completedDates.isEmpty else { return 0 }
+                
+                let calendar = Calendar.current
+                // Tarihleri gün başlangıcına normalize et ve benzersiz yap (Set kullanarak)
+                let uniqueDates = Set(completedDates.map { calendar.startOfDay(for: $0) })
+                let sortedDates = uniqueDates.sorted(by: >) // En yeni tarihten eskiye
+                
+                var streak = 0
+                var checkDate = calendar.startOfDay(for: Date()) // Bugünden geriye kontrol başlasın
+                
+                for date in sortedDates {
+                    if date == checkDate {
+                        streak += 1
+                        checkDate = calendar.date(byAdding: .day, value: -1, to: checkDate)!
+                    } else if date < checkDate {
+                        break // Seri bozuldu
+                    }
+                }
+                return streak
+            }
+            
+            
+            var isCompletedToday: Bool {
+                let calendar = Calendar.current
+                return completedDates.contains { calendar.isDateInToday($0) }
+            
+        
     }
 }
